2206770047ef0c614503831e4ce7ec18bf716c6f
msupetran <msupetran@blastasia.com>
2016-01-30 17:21:06 +0800

    Added TipidPcUnitOfWork.cs file.

---
+++ TipidPc.Infrastructure/Data/TpcUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Common.Infrastructure.Domain;
+using TipidPc.Infrastructure.Domain;
+
+namespace TipidPc.Infrastructure.Data
+{
+    public class TpcUnitOfWork : UnitOfWorkBase<ITpcContext>
+    {
+        // Constructors
+        public TpcUnitOfWork(ITpcContext context) : base(context) { }
+
+        // Methods implemented from UnitOfWorkBase<T>
+        public override IRepository<TEntity> GetRepository<TEntity>()
+        {
+            return new TpcRepository<TEntity>(this.Context);
+        }
+    }
+}
