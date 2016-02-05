a66a3a961c1b0f6aca541ff6206b5450235af183
Marlon Supetran <msupetran@blastasia.com>
2016-02-05 21:05:44 +0800

    Added ItemsController.

---
+++ TipidPc.Presentation.WebApi/Controllers/ItemsController.cs
@@ -0,0 +1,71 @@
+﻿using Common.Infrastructure.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TipidPc.Domain;
+using TipidPc.Domain.Models;
+using TipidPc.Presentation.WebApi.Models;
+
+namespace TipidPc.Presentation.WebApi.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        // Fields
+        private ItemDomainService _itemDomainService;
+
+        // Constructors
+        public ItemsController(IRepository<Item> _itemRepository)
+        {
+            _itemDomainService = new ItemDomainService(_itemRepository);
+        }
+
+        // Methods
+        public IHttpActionResult Get(int id = 0)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    var items = _itemDomainService.QueryItems();
+                    var itemsViewModels = new List<ItemsViewModel>();
+                    foreach (var i in items)
+                    {
+                        itemsViewModels.Add(new ItemsViewModel()
+                        {
+                            ID = i.Id,
+                            Section = i.Section,
+                            Name = i.Header.Title,
+                            Category = i.Category,
+                            Amount = i.Amount,
+                            Condition = i.Condition,
+                            Warranty = i.Warranty,
+                            Duration = i.Duration,
+                            Description = i.Entry.Message
+                        });
+                    }
+                    if (itemsViewModels != null && itemsViewModels.Count() > 0)
+                    {
+                        return Ok(itemsViewModels); 
+                    }
+                }
+
+                var item = _itemDomainService.QueryItemById(id);
+                if (item != null)
+                {
+                    return Ok(item);
+                }
+
+                // else...
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log errors...
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
---
+++ TipidPc.Presentation.WebApi/Models/ItemsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TipidPc.Domain.Models;
+
+namespace TipidPc.Presentation.WebApi.Models
+{
+    public class ItemsViewModel
+    {
+        public int ID { get; set; }
+        public ItemSection Section { get; set; }
+        public string Name { get; set; }
+        public Category Category{ get; set; }
+        public decimal Amount { get; set; }
+        public ItemCondition Condition { get; set; }
+        public ItemWarranty Warranty { get; set; }
+        public ItemDuration Duration { get; set; }
+        public string Description { get; set; }
+    }
+}
